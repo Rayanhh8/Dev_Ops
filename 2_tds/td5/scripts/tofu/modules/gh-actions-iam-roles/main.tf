dad52a62b818f408b45acb11715c78394d04f4f3
Rayan <rayan.abdelmeguid@gmail.com>
2026-01-17 15:32:53 +0000

    Fix apply role OIDC sub condition

--- 2_tds/td5/scripts/tofu/modules/gh-actions-iam-roles/main.tf
+++ 2_tds/td5/scripts/tofu/modules/gh-actions-iam-roles/main.tf
@@ -172,9 +172,9 @@ data "aws_iam_policy_document" "assume_role_policy_for_apply" {
     condition {
       test     = "StringEquals"
       variable = "token.actions.githubusercontent.com:sub"
-      values = ["repo:${var.github_repo}:*"]
-
+      values   = ["repo:${var.github_repo}:ref:refs/heads/main"]
     }
+
 
     condition {
       test     = "StringEquals"
