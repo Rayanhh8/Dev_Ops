174d62d261770b6289fbc1eae6a8d3efc40da5a9
Rayan <rayan.abdelmeguid@gmail.com>
2026-01-17 16:55:23 +0000

    Fix: allow tests role to manage inline role policies

--- 2_tds/td5/scripts/tofu/modules/gh-actions-iam-roles/main.tf
+++ 2_tds/td5/scripts/tofu/modules/gh-actions-iam-roles/main.tf
@@ -49,7 +49,10 @@ data "aws_iam_policy_document" "test_serverless_app" {
       "iam:UpdateRole",
       "iam:PassRole",
       "iam:List*Role*",
-      "iam:Get*Role*"
+      "iam:Get*Role*",
+      "iam:PutRolePolicy",
+    "iam:DeleteRolePolicy"
+
     ]
     resources = ["arn:aws:iam::*:role/${var.lambda_base_name}*"]
   }
