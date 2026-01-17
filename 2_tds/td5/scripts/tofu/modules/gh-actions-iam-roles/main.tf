e1d9ffd614d9479b6f37aa8b4a523f826f9bcb3f
Rayan <rayan.abdelmeguid@gmail.com>
2026-01-17 14:11:12 +0000

    Fix OIDC trust policy to allow PR branches

--- 2_tds/td5/scripts/tofu/modules/gh-actions-iam-roles/main.tf
+++ 2_tds/td5/scripts/tofu/modules/gh-actions-iam-roles/main.tf
@@ -154,7 +154,8 @@ data "aws_iam_policy_document" "assume_role_policy_for_apply" {
     condition {
       test     = "StringEquals"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:${var.github_repo}:ref:refs/heads/main"]
+      values = ["repo:${var.github_repo}:*"]
+
     }
 
     condition {
